f76423d2aec69efaa5643465d5ba1bb037af1c59
mmanchidi <mmanchidir@gmail.com>
2023-06-20 09:33:48 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int r;
+
+	for (r = 0; r < 10; r++)
+		printf("%d", r);
+	printf("\n");
+
+	return (0);
+}

9c6beed2759ccae229f141d2c33838c95853f0f9
mmanchidi <mmanchidir@gmail.com>
2023-06-20 09:07:03 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - Entry point
- * Return: 0 (Success)
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char low;

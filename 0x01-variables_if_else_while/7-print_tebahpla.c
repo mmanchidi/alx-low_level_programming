86b362d6b7d73ef47593084e6aae8b07367c95d2
mmanchidi <mmanchidir@gmail.com>
2023-06-20 10:05:05 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'z'; low = 'a'; low++)
+		putchar(low);
+	putchar('\n');
+
+	return (0);
+}

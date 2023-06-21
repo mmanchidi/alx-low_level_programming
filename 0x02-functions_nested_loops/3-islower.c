6183fd13a23a72f774c0be42e02875494a7cd872
mmanchidi <mmanchidir@gmail.com>
2023-06-21 12:50:38 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _islower - Print alphabet in lowercase 10 times
+ * @c: An input character
+ *
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+int _islower(int r)
+{
+	char i;
+	int lower = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == r)
+			lower = 1;
+	}
+
+	return (lower);
+}

a4c113f331b647f99b93933f42662d67368b60c7
mmanchidi <mmanchidir@gmail.com>
2023-06-22 12:00:46 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,20 +3,20 @@ void print_integer(int a);
 
 /**
  * print_number - a function that prints an integer.
- * @b: An input integer
+ * @n: An input integer
  * Return: Nothing
  */
-void print_number(int b)
+void print_number(int n)
 {
-	if (b == 0)
+	if (n == 0)
 		_putchar('0');
-	else if (b < 0)
+	else if (n < 0)
 	{
 		_putchar('-');
-		print_integer(b * -1);
+		print_integer(n * -1);
 	}
 	else
-		print_integer(b);
+		print_integer(n);
 }
 
 /**

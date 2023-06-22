1a4c28d4b099a878ad8440b6206b011350fee9c2
mmanchidi <mmanchidir@gmail.com>
2023-06-22 11:24:42 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry piont
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 1;
+
+	for (; n < 100 ; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+			printf("FizzBuzz ");
+		else if (n % 3 == 0)
+			printf("Fizz ");
+		else if (n % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", n);
+	}
+	printf("Buzz\n");
+
+	return (0);
+}

638067455cadff36133bbf52580383af5604f30a
mmanchidi <mmanchidir@gmail.com>
2023-06-26 14:11:46 -0700

    Half done

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,18 +3,14 @@
 /**
  * _puts - a function that prints a string, followed
  * by a new line, to stdout.
- * @str: an iput string
- * Return: Nothing
+ * @str: string to print
  */
 
 void _puts(char *str)
 {
-	int i = 0;
-
-	while (str[i] != '\0')
+	while (*str != '\0')
 	{
-		_putchar(str[i]);
-		i++;
+		_putchar(*str++);
 	}
-	_putchar('\n');
+_putchar('\n');
 }

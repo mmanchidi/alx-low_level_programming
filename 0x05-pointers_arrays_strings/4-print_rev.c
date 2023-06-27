cc8fffcfc2e7a68db2f741ec0142a37ddc15cf41
mmanchidi <mmanchidir@gmail.com>
2023-06-27 11:59:54 -0700

    Half done

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
-/** print_rev - a function that prints a string, in reverse,
- * followed by a new line.
+/**
+ * print_rev - a function that prints a string, in reverse
  * @s: an input string
  * Return: Nothing
  */
